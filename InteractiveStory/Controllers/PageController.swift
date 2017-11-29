33c9fbd1067d3f30dccfe2c3bdf5988d910bd919
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-11-29 12:46:11 -0500

    Implement constraints for artwork and story elements

--- InteractiveStory/Controllers/PageController.swift
+++ InteractiveStory/Controllers/PageController.swift
@@ -12,19 +12,46 @@ class PageController: UIViewController {
     
     var page: Page?
     
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
+    // MARK: - User Interface Properties
+    
+    let artworkView = UIImageView()
+    let storyLabel = UILabel()
+    let firstChoiceButton = UIButton(type: .system)
+    let secondChoiceButton = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let page = page {
+            artworkView.image = page.story.artwork
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // MARK: - Constraints for Artwork view
+        
+        view.addSubview(artworkView)
+        artworkView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            artworkView.topAnchor.constraint(equalTo: view.topAnchor),
+            artworkView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            artworkView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            artworkView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        // MARK: - Constraints for Story label
+        
+        view.addSubview(storyLabel)
+        storyLabel.numberOfLines = 0
+        storyLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            storyLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
+            storyLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
+            storyLabel.topAnchor.constraint(equalTo: view.centerYAnchor, constant: -48.0)
+        ])
     }
-
 }
