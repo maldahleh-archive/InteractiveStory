5e57348ea7830d20d2782b9efe62c615ee633b95
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-11-29 17:31:42 -0500

    Implement keyboard constraint modification

--- InteractiveStory/Controllers/ViewController.swift
+++ InteractiveStory/Controllers/ViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var textFieldBottomConstraint: NSLayoutConstraint!
+    
+    // MARK: - Initalization and deinitialization methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    // MARK: - Segue to story
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier != "startAdventure" { return }
@@ -23,23 +38,44 @@ class ViewController: UIViewController {
             
             pageController.page = Adventure.story(withName: name)
         } catch AdventureError.noNameEntered {
-            let alertController = createAlertControllerWith(title: "No Name Provided", message: "Please provide a name.")
-            
-            present(alertController, animated: true, completion: nil)
+            presentAlertWith(title: "No Name Provided", message: "Please provide a name.")
         } catch let error {
-            let alertController = createAlertControllerWith(title: "Unknown Error", message: "\(error.localizedDescription)")
-            
-            present(alertController, animated: true, completion: nil)
+            presentAlertWith(title: "Unknown Error", message: "\(error.localizedDescription)")
         }
     }
     
-    func createAlertControllerWith(title: String, message: String) -> UIAlertController {
+    // MARK: - Keyboard
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if let info = notification.userInfo, let keyboardFrame = info[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let frame = keyboardFrame.cgRectValue
+            textFieldBottomConstraint.constant = frame.size.height + 10
+            
+            UIView.animate(withDuration: 0.8) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        textFieldBottomConstraint.constant = 40
+        
+        UIView.animate(withDuration: 0.8) {
+            self.view.layoutIfNeeded()
+        }
+        
+        return true
+    }
+    
+    // MARK: - Helper methods
+    
+    func presentAlertWith(title: String, message: String) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alertController.addAction(action)
         
-        return alertController
+        present(alertController, animated: true, completion: nil)
     }
 }
-
